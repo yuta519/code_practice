9b65a3183690c4e917de69d1210539df5591fe62
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-10-08 01:01:28 +0900

    add merged linked list

--- linked_list.py
+++ linked_list.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-from tkinter import W
 
 class Node:
     def __init__(self, data: int, next_node:Node = None) -> None:
@@ -30,15 +29,64 @@ class LinkedList:
         self.head = new_node
 
 
-if __name__ == "__main__":
-    linked_list = LinkedList()
-    linked_list.append(1)
-    linked_list.append(2)
-    linked_list.append(3)
-    linked_list.insert(0)
+""""
+Given two sorted linked lists, merge them so that the resulting linked list is also sorted. Consider two sorted linked lists and the merged list below them as an example.
 
-    print(linked_list)
-    print(linked_list.head.data)
-    print(linked_list.head.next.data)
-    print(linked_list.head.next.next.data)
-    print(linked_list.head.next.next.next.data)
+head1 [4, 8, 15, 19] none
+head2 [7, 9, 10, 16] none
+
+head  [4, 7, 8, 9, 10, 15, 16, 19] none
+"""
+
+if __name__ == "__main__":
+    head1 = LinkedList()
+    head2 = LinkedList()
+    for i in [4, 8, 15, 19]:
+        head1.append(i)
+    for i in [7, 9, 10, 16]:
+        head2.append(i)
+
+    merged_linked_list = LinkedList()
+
+    current_head1 = head1.head
+    current_head2 = head2.head
+
+    while current_head1.next or current_head2.next:
+        last_node = merged_linked_list.head
+
+        if merged_linked_list.head is None:
+            if current_head1.data < current_head2.data:
+                merged_linked_list.head = Node(current_head1.data)
+                current_head1 = head1.head.next
+            else:
+                merged_linked_list.head = Node(current_head2.data)
+                current_head2 = head2.head.next
+        else:
+            while last_node.next:
+                last_node = last_node.next
+
+            if current_head1.data < current_head2.data:
+                last_node.next = Node(current_head1.data)
+                current_head1 = current_head1.next
+            else:
+                last_node.next = Node(current_head2.data)
+                current_head2 = current_head2.next
+
+    while last_node.next:
+        last_node = last_node.next
+
+    if current_head1.data < current_head2.data:
+        last_node.next = Node(current_head1.data)
+        last_node.next.next = Node(current_head2.data)
+    else:
+        last_node.next = Node(current_head2.data)
+        last_node.next.next = Node(current_head1.data)
+
+    print(merged_linked_list.head.data)
+    print(merged_linked_list.head.next.data)
+    print(merged_linked_list.head.next.next.data)
+    print(merged_linked_list.head.next.next.next.data)
+    print(merged_linked_list.head.next.next.next.next.data)
+    print(merged_linked_list.head.next.next.next.next.next.data)
+    print(merged_linked_list.head.next.next.next.next.next.next.data)
+    print(merged_linked_list.head.next.next.next.next.next.next.next.data)
