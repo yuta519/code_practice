ce38775ac5bf4cc886ffe61cd064a57f4bb18f51
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-10-10 22:46:25 +0900

    add solution

--- can_segment_string.py
+++ can_segment_string.py
@@ -3,10 +3,17 @@ You are given a dictionary of words and a large input string. You have to find o
 """
 
 def can_segment_string(s, dictionary):
-    #TODO: Write - Your - Code
-    return
+    for i in range(1, len(s)+1):
+        if s[0:i] in dictionary:
+            first_str_point = i
+            if s[first_str_point:] in dictionary:
+                return True
+    return False
 
 
 if __name__ == "__main__":
     dictionary_words: list[str] = ["apple", "apple", "pear", "pie"]
-    input_string: str =  "applepie"
+    input_string1: str =  "applepie"
+    input_string2: str =  "applepeer"
+    print(can_segment_string(input_string1, dictionary_words))
+    print(can_segment_string(input_string2, dictionary_words))
