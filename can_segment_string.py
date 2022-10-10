03ee96745ada5720e02b97991091cf4afdecaf0e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-10-10 15:25:50 +0900

    Initial Commit

---
+++ can_segment_string.py
@@ -0,0 +1,12 @@
+"""
+You are given a dictionary of words and a large input string. You have to find out whether the input string can be completely segmented into the words of a given dictionary. The following two examples elaborate on the problem further.
+"""
+
+def can_segment_string(s, dictionary):
+    #TODO: Write - Your - Code
+    return
+
+
+if __name__ == "__main__":
+    dictionary_words: list[str] = ["apple", "apple", "pear", "pie"]
+    input_string: str =  "applepie"
