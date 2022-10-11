aa0df4a510628fdd695cfb3ae9213dac9a948441
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-10-11 15:17:06 +0900

    Initial commit

---
+++ reverse_words.py
@@ -0,0 +1,9 @@
+"""
+Reverse the order of words in a given sentence(an array of characters).
+"Hello World" -> "World Hello"
+"""
+
+
+def reverse_words(sentence):    # sentence here is an array of characters
+  #TODO: Write - Your - Code
+  return sentence
