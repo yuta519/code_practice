177b6e5833a8d3a64dc2b03bcaca7035842022f7
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-10-15 18:12:45 +0900

    chore

--- reverse_words.py
+++ reverse_words.py
@@ -4,6 +4,23 @@ Reverse the order of words in a given sentence(an array of characters).
 """
 
 
-def reverse_words(sentence):    # sentence here is an array of characters
-  #TODO: Write - Your - Code
-  return sentence
+def reverse_words(sentence: list[str]) -> str:    # sentence here is an array of characters
+    #TODO: Write - Your - Code
+    center: int = len(sentence) // 2 if len(sentence) % 2 == 1 else None
+
+    reversed_sentence: list[str] = []
+
+    if center is None:
+        reverse_index: int = -1
+        for i in range(0, len(sentence)):
+
+            pass
+
+    else:
+        for i in range(0, len(sentence)):
+            pass
+
+        pass
+
+
+    return sentence
